eddf69eea06611030453bf90c2e04a49e37d8d2b
n/a <n/a>
2023-04-27 12:53:43 +0100

    Moved the digital assistant's video HTML generation back to chatbot.js

--- flask/static/js/chatbot/tutorials.js
+++ flask/static/js/chatbot/tutorials.js
@@ -1,29 +1,9 @@
-// BOT_UI.message.add({
-//     type: 'html',
-//     content: `<video src="static/tutorials/file.mp4" height="200" width="300" controls></video>`
-// });
-
-
-
 function getTutorial() {
   const REQUEST = new XMLHttpRequest();
 
   REQUEST.addEventListener('load', function (_) {
     const SUBDIRECTORIES = JSON.parse(REQUEST.response);
     const NUM_OF_SUBDIRECTORIES = SUBDIRECTORIES.length;
-
-    const ACTIONS = [];
-
-    for (let i=0; i < NUM_OF_SUBDIRECTORIES; i++) {
-      ACTIONS.push({
-        'text': SUBDIRECTORIES[i],
-        'value': 'tuts'
-      });
-    }
-
-    BOT_UI.action.button({
-        action: ACTIONS
-    });
   });
 
   REQUEST.open('POST', '/tutorials');
