253d6431ed77e1501f03c69467ee00536327c25f
n/a <n/a>
2023-04-27 12:00:09 +0100

    Replaced database configurations.

--- flask/global_modules/config.py
+++ flask/global_modules/config.py
@@ -1,15 +1,12 @@
 SECRET_KEY = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-# mongoengine_URI = 'mongodb://nitrexo:mongonitrexopassword@mongodb:27017/?authSource=admin' # production
-mongoengine_URI = 'mongodb://localhost:27017/' # development
-mongoengine_DB = 'nitrexo'
-
-
-# pymongo_URI = 'mongodb://nitrexo:mongonitrexopassword@mongodb:27017/' # production
-pymongo_URI = 'mongodb://localhost:27017' # development
-pymongo_DB = 'nitrexo'
-pymongo_MESSAGES_COLLECTION = 'messages'
+# the following are all specified in docker-compose.yml in the root folder
+DB_HOSTNAME = 'nitrexodb'
+DB_USER = 'root'
+DB_PASSWORD = 'NitroZiko83r'
+MONGO_SERVICE_NAME = 'mongo'
+MONGO_SERVICE_PORT = 27017
 
 
 UPLOAD_FOLDER = '/uploads'
