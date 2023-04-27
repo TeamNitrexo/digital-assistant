a02180475a24aa542cc1fe081adfd83e05b54a05
n/a <n/a>
2023-04-27 13:58:28 +0100

    Tutorials can now be retrieved.

--- flask/static/js/chatbot/tutorials.js
+++ flask/static/js/chatbot/tutorials.js
@@ -2,20 +2,46 @@ import { BOT_UI } from "./chatbot.js";
 
 
 
-function getTutorial() {
-  let path_to_tutorial_video = '/';
+let path_to_tutorial_video = '';
 
+function getTutorial() {
   const REQUEST = new XMLHttpRequest();
 
   REQUEST.addEventListener('load', function (_) {
-    const SUBDIRECTORIES = JSON.parse(REQUEST.response);
-    const NUM_OF_SUBDIRECTORIES = SUBDIRECTORIES.length;
+    const OPTIONS = JSON.parse(REQUEST.responseText);
+    const NUM_OF_OPTIONS = OPTIONS.length;
+
+    const ACTIONS = [];
+
+    for (let i=0; i < NUM_OF_OPTIONS; i++) {
+      const OPTION = OPTIONS[i];
+
+      ACTIONS.push({
+        'text': OPTION,
+        'value': OPTION
+      });
+    }
+
+    BOT_UI.action.button({
+      action: ACTIONS
+    }).then(function (response) {
+      path_to_tutorial_video += '/' + response.value;
+
+      if (/.mp4$/.test(path_to_tutorial_video)) {
+        BOT_UI.message.add({
+            type: 'html',
+            content: `<video src="static/tutorials/${path_to_tutorial_video}" height="200" width="300" controls></video>`
+        });
+      }
+      else {
+        getTutorial();
+      }
+    });
   });
 
   REQUEST.open('POST', '/tutorials');
-  REQUEST.send();
-
-  return path_to_tutorial_video;
+  REQUEST.setRequestHeader('content-type', 'application/json');
+  REQUEST.send(JSON.stringify(path_to_tutorial_video));
 };
 
 export {
