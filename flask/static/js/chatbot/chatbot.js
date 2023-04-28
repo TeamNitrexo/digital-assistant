c2486e0ee2b53c00327ad337f18ef79592a004f2
n/a <n/a>
2023-04-28 10:13:29 +0100

    Fixed formatting.

--- flask/static/js/chatbot/chatbot.js
+++ flask/static/js/chatbot/chatbot.js
@@ -7,49 +7,49 @@ const BOT_UI = new BotUI('nitrexo-bot');
 
 function init_chatbot() {
   BOT_UI.message.add({
-      content: 'Hi, I am Nitrexo\'s Digital Assistant'
+    content: 'Hi, I am Nitrexo\'s Digital Assistant'
   }).then(function () {
-      BOT_UI.message.add({
-          delay: 1000,
-          content: 'How can I help you?'
-      });
+    BOT_UI.message.add({
+      delay: 1000,
+      content: 'How can I help you?'
+    });
 
-      suggestedResponses(2000);
+    suggestedResponses(2000);
   });
 };
 init_chatbot();
 
 function suggestedResponses(delay) {
-    setTimeout(() => {
-        BOT_UI.action.button({
-            action: [
-                {
-                'text':'What is Thermo-elastic Verification?',
-                'value':'tev'
-                },
-                {
-                'text':'Can you perform Temperature Mapping using SINAS?',
-                'value':'perf-tmp'
-                },
-                {
-                'text':'What is Thermal Model Reduction?',
-                'value':'tmr'
-                },
-                {
-                'text':'Can you perform Thermal Model Reduction using PythonOCC?',
-                'value':'perf-tmr'
-                },
-                {
-                'text':'Can I see the tutorials?',
-                'value':'tuts'
-                },
-            ]
-        }).then(function (response) {
-            if (response.value === 'tuts') {
-              getTutorial();
-            }
-        });
-    }, delay);
+  setTimeout(() => {
+    BOT_UI.action.button({
+      action: [
+        {
+          'text': 'What is Thermo-elastic Verification?',
+          'value': 'tev'
+        },
+        {
+          'text': 'Can you perform Temperature Mapping using SINAS?',
+          'value': 'perf-tmp'
+        },
+        {
+          'text': 'What is Thermal Model Reduction?',
+          'value': 'tmr'
+        },
+        {
+          'text': 'Can you perform Thermal Model Reduction using PythonOCC?',
+          'value': 'perf-tmr'
+        },
+        {
+          'text': 'Can I see the tutorials?',
+          'value': 'tuts'
+        },
+      ]
+    }).then(function (response) {
+      if (response.value === 'tuts') {
+        getTutorial();
+      }
+    });
+  }, delay);
 };
 
 function provideSuggestionsAgain() {
