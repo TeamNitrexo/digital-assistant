2bb4db70022eb5da20c0021119979f9106fe7ff8
n/a <n/a>
2023-04-28 11:38:52 +0100

    Deleted 'knowledge' collection. Added a documentation to explain the new way to implement chatbot responses.

--- flask/global_modules/database.py
+++ flask/global_modules/database.py
@@ -2,9 +2,22 @@ from mongoengine import connect
 from .config import DB_HOSTNAME, DB_USER, DB_PASSWORD, MONGO_SERVICE_NAME, MONGO_SERVICE_PORT
 from pymongo import MongoClient
 
+"""
+NOTE:
+#1
+You can find the chatbot's answers to thermal-related questions in 'tmr/data/Knowledge Base.csv'
+
+#2
+The 'messages' collection stores the responses for the chatbot (this is being changed because it's too messy).
+
+The newer implementation uses JavaScripts's `XMLHttpRequest` to make requests to the backend routes. This sometimes involves passing JSON back and forth or just to one.
+
+Please look at the JS files in 'static/js/chatbot' for examples on how to do this.
+
+"""
+
 DB_NAME = 'nitrexo'
-COLLECTION_1_NAME = 'messages' # chatbot responses (OLD IMPLEMENTATION)
-COLLECTION_2_NAME = 'knowledge' # chatbot answers to thermal-related questions; Q&A are in 'tmr/data/Knowledge Base.csv' (NEW IMPLEMENTATION)
+COLLECTION_1_NAME = 'messages'
 
 mongo_client = MongoClient(
     host=DB_HOSTNAME,
@@ -15,6 +28,5 @@ mongo_client = MongoClient(
 )
 db = mongo_client[DB_NAME]
 col_messages = db[COLLECTION_1_NAME]
-col_knowledge = db[COLLECTION_2_NAME]
 
 connect(host=f"mongodb://{DB_USER}:{DB_PASSWORD}@{MONGO_SERVICE_NAME}:{MONGO_SERVICE_PORT}/{DB_NAME}?authSource=admin")
